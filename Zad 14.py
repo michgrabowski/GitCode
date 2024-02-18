ee1604393367ad288e45396c3bc9676544b5e61c
michg.grabowski <michg.grabowski@gmail.com>
2024-02-18 13:13:22 +0100

    Second commit

---
+++ Zad 14.py
@@ -0,0 +1,14 @@
+print ("Program") 
+
+rok1 = input("Podaj diwe piersze cyfry aktualnego roku: ")
+rok2 = input("Podaj diwe ostatnie cyfry aktualnego roku: ")
+
+print ("Mamy rok: ", rok1 + rok2) 
+
+wiek = int(input("Podaj swój wiek: "))
+
+print("Masz lat:", wiek)
+
+data = int(rok1+rok2)
+
+print ("Twój rok urodzenia do wyświetlenia to: ", data - wiek)
